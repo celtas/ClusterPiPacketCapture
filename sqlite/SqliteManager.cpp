8f51ebaef7d48c6faa211bc149b2f7c1ca667a6e
celtas <cell_tas@yahoo.co.jp>
2016-11-04 01:05:36 +0900

    sqliteの導入

---
+++ sqlite/SqliteManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by System Administrator on 11/3/16.
+//
+
+#include "SqliteManager.h"
---
+++ sqlite/SqliteManager.h
@@ -0,0 +1,15 @@
+//
+// Created by System Administrator on 11/3/16.
+//
+
+#ifndef CIT_MONOMAKE_SQRITEMANAGER_H
+#define CIT_MONOMAKE_SQRITEMANAGER_H
+
+
+class SqliteManager {
+
+
+};
+
+
+#endif //CIT_MONOMAKE_SQRITEMANAGER_H
